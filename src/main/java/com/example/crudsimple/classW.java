341a24494371b56bd7a01f8524811c6152413677
Juan Carlos A <abjuancarlos.12@gmail.com>
2024-01-07 01:59:22 -0500

    added new structure of project DDD architecture

--- src/main/java/com/example/crudsimple/classW.java
+++ src/main/java/com/example/crudsimple/classW.java
@@ -4,8 +4,11 @@ package com.example.crudsimple;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @RestController
+@RequestMapping("/task")
 public class classW {
     @GetMapping("/")
     public String index() {
--- src/main/java/com/example/crudsimple/models/products.java
+++ src/main/java/com/example/crudsimple/models/products.java
@@ -10,5 +10,5 @@ public class products {
 
     public String name;
 
-
+    
 }
